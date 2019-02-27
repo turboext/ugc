6aa9f4747532f083ab82853ac03f8cefe2c48dbe
archid <archid@yandex-team.ru>
2019-02-27 12:46:20 +0300

    Add tslint whitespace rule

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,12 +1,12 @@
-import {IRequest} from "../utils/request";
-import {NextFunction, Response} from "express";
+import { IRequest } from '../utils/request';
+import { NextFunction, Response } from 'express';
 
-export function cors(origin: string = "*") {
+export function cors(origin: string = '*') {
 
     return (req: IRequest, res: Response, next: NextFunction) => {
-        res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Credentials", "true");
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Credentials', 'true');
 
         next();
     };
